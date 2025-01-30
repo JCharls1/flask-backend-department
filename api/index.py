6d1fae79bca593fd0c0753ed18ee6ad494b97632
C1OX <151930727+JCharls1@users.noreply.github.com>
2025-01-30 16:01:41 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -338,14 +338,3 @@ def move_employee_to_new_department():
 
 if __name__ == '__main__':
     app.run()
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
